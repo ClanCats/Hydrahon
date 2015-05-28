f938343f3f987e2ad5933ed00b34ab0449c54cb6
Mario Döring <deluna.yy@gmail.com>
2015-05-29 00:11:11 +0200

    Spaces instead of tabs

--- src/Builder.php
+++ src/Builder.php
@@ -3,89 +3,89 @@
 /**
  * Query Builder manager
  ** 
- * @package 		Hydrahon
- * @copyright 		2015 Mario Döring
+ * @package         Hydrahon
+ * @copyright       2015 Mario Döring
  */
 
 class Builder
-{	
-	/**
-	 * Array of available query grammers
-	 * 
-	 * @var array
-	 */
-	protected static $grammar = array(
+{   
+    /**
+     * Array of available query grammers
+     * 
+     * @var array
+     */
+    protected static $grammar = array(
 
-		// MySQL
-		'mysql' => array(
-			'ClanCats\\Hydrahon\\Query\\Sql',
-			'ClanCats\\Hydrahon\\Translator\\Mysql',
-		),
+        // MySQL
+        'mysql' => array(
+            'ClanCats\\Hydrahon\\Query\\Sql',
+            'ClanCats\\Hydrahon\\Translator\\Mysql',
+        ),
 
-		// SQLite
-		'sqlite' => array(
-			'ClanCats\\Hydrahon\\Query\\Sql',
-			'ClanCats\\Hydrahon\\Translator\\Sqlite',
-		),
+        // SQLite
+        'sqlite' => array(
+            'ClanCats\\Hydrahon\\Query\\Sql',
+            'ClanCats\\Hydrahon\\Translator\\Sqlite',
+        ),
 
-		// MongoDB
-		'mongo' => array(
-			'ClanCats\\Hydrahon\\Query\\MongoDB',
-			'ClanCats\\Hydrahon\\Translator\\MongoDB',
-		),
-	);
+        // MongoDB
+        'mongo' => array(
+            'ClanCats\\Hydrahon\\Query\\MongoDB',
+            'ClanCats\\Hydrahon\\Translator\\MongoDB',
+        ),
+    );
 
-	/**
-	 * Extend the query builder by a new grammar
-	 * 
-	 * @throws ClanCats\Hydrahon\Exception
-	 * 
-	 * @param string 				$grammerKey
-	 * @param string				$queryBuilder
-	 * @param string 				$queryTranslator
-	 * @return void
-	 */
-	public static function extend( $grammerKey, $queryBuilder, $queryTranslator )
-	{
-		if ( isset( static::$grammar[$grammerKey] ) )
-		{
-			throw new Exception( 'Cannot overwrite Hydrahon grammer.' );
-		}
+    /**
+     * Extend the query builder by a new grammar
+     * 
+     * @throws ClanCats\Hydrahon\Exception
+     * 
+     * @param string                $grammerKey
+     * @param string                $queryBuilder
+     * @param string                $queryTranslator
+     * @return void
+     */
+    public static function extend( $grammerKey, $queryBuilder, $queryTranslator )
+    {
+        if ( isset( static::$grammar[$grammerKey] ) )
+        {
+            throw new Exception( 'Cannot overwrite Hydrahon grammer.' );
+        }
 
-		static::$grammar[$grammerKey] = array( $queryBuilder, $queryTranslator );
-	}
+        static::$grammar[$grammerKey] = array( $queryBuilder, $queryTranslator );
+    }
 
-	/**
-	 * Currently loaded query builder
-	 * 
-	 * @var ClanCats\Hydrahon\Query
-	 */
-	protected $queryBuilder = null;
+    /**
+     * Currently loaded query builder
+     * 
+     * @var ClanCats\Hydrahon\Query
+     */
+    protected $queryBuilder = null;
 
-	/**
-	 * Currently loaded query translator
-	 * 
-	 * @var ClanCats\Hydrahon\TranslatorInterface
-	 */
-	protected $queryTranslator = null;
+    /**
+     * Currently loaded query translator
+     * 
+     * @var ClanCats\Hydrahon\TranslatorInterface
+     */
+    protected $queryTranslator = null;
 
-	/**
-	 * Create a new Hydrahon builder instance using the giving grammar
-	 *
-	 * @throws ClanCats\Hydrahon\Exception
-	 * 
-	 * @param string 				$grammerKey
-	 * @return void
-	 */
-	public function __construct( $grammerKey )
-	{
-		if ( isset( $this->grammar[$grammerKey] ) )
-		{
-			throw new Exception( 'There is no Hydrahon grammer "'.$grammerKey.'" registered.' );
-		}
+    /**
+     * Create a new Hydrahon builder instance using the giving grammar
+     *
+     * @throws ClanCats\Hydrahon\Exception
+     * 
+     * @param string                $grammerKey
+     * @return void
+     */
+    public function __construct( $grammerKey )
+    {
+        if ( isset( $this->grammar[$grammerKey] ) )
+        {
+            throw new Exception( 'There is no Hydrahon grammer "'.$grammerKey.'" registered.' );
+        }
 
-		
-	}
+        
+    }
 
-	
+    
 }
--- src/Query.php
+++ src/Query.php
@@ -3,34 +3,34 @@
 /**
  * Query object
  ** 
- * @package 		Hydrahon
- * @copyright 		2015 Mario Döring
+ * @package         Hydrahon
+ * @copyright       2015 Mario Döring
  */
 
 class Query
-{	
-	/**
-	 * The database the query should be executed on
-	 * 
-	 * @var string
-	 */
-	protected $database = null;
+{   
+    /**
+     * The database the query should be executed on
+     * 
+     * @var string
+     */
+    protected $database = null;
 
-	/**
-	 * The table the query should be executed on
-	 * 
-	 * @var string
-	 */
-	protected $table = null;
+    /**
+     * The table the query should be executed on
+     * 
+     * @var string
+     */
+    protected $table = null;
 
-	/**
-	 * Construct a new builder with given nodes
-	 *
-	 * @param array 				$nodes
-	 * @return void
-	 */
-	public function __construct( array $nodes )
-	{
-		
-	}
+    /**
+     * Construct a new builder with given nodes
+     *
+     * @param array                 $nodes
+     * @return void
+     */
+    public function __construct( array $nodes )
+    {
+        
+    }
 }
