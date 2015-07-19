491ccd0e7b714badcff6f761287bb4270d1b0f9b
Mario Döring <deluna.yy@gmail.com>
2015-07-19 12:07:03 +0200

    Added expression text case

---
+++ tests/Query/Expression.php
@@ -0,0 +1,37 @@
+<?php namespace ClanCats\Hydrahon\Test;
+/**
+ * Hydrahon builder test 
+ ** 
+ *
+ * @package 		Hydrahon
+ * @copyright 		Mario Döring
+ *
+ * @group Hydrahon
+ * @group Hydrahon_Query
+ * @group Hydrahon_Query_Expression
+ */
+
+use ClanCats\Hydrahon\Query\Expression;
+
+class Expression_Test extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Expression::construct test
+	 */
+	public function testConstruct()
+	{
+		$expression = new Expression('foo');
+		$this->assertInstanceOf('ClanCats\\Hydrahon\\Query\\Expression', $expression);
+	}
+
+	/**
+	 * Expression::value test
+	 */
+	public function testValue()
+	{
+		$expression = new Expression('foo');
+
+		$this->assertEquals('foo', (string)$expression);
+		$this->assertEquals('foo', $expression->value());
+	}
+}
