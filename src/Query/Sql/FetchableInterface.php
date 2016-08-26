4cc74bca61f065118663b6e4a0f520cc4aa958d0
Mario Döring <deluna.yy@gmail.com>
2016-08-26 10:18:37 +0200

    Added fetchable interface

---
+++ src/Query/Sql/FetchableInterface.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace ClanCats\Hydrahon\Query\Sql;
+
+/**
+ * Does currently nothing itself but helps identifying queries that
+ * can / should be fetched.
+ **
+ * @package         Hydrahon
+ * @copyright       2015 Mario Döring
+ */
+
+interface FetchableInterface {}
